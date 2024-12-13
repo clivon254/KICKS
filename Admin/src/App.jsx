1726a0d337b16a9113756ca4cc0a82b6e5071cf2
Clivon Osire <clivon254@icloud.com>
2024-12-13 06:52:06 +0300

    amefanya

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,17 +1,79 @@
-import { useState } from 'react'
 
-
+import { useContext, useState } from 'react'
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router-dom"
+import {Toaster} from "sonner"
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+import ResetPassword from './pages/ResetPassword'
+import { useSelector } from 'react-redux'
+import { StoreContext } from './context/store'
+import Header from './components/Header'
 
 function App() {
  
+  const Layout = () => {
+
+    const {currentUser} = useSelector(state => state.user)
+
+    const {token} = useContext(StoreContext)
+
+    return (
+
+      currentUser?.isAdmin ?
+
+        <div className="w-full min-h-screen">
+
+          <div className="">
+
+            <Header/>
+            
+          </div>
+
+          <div className="">
+
+            <Outlet/>
+
+          </div>
+
+        </div>
+        :
+        <Navigate to="/sign-in" />
+
+    )
+
+  }
 
   return (
    
-    <div className="">
+    <BrowserRouter>
+    
+      <Toaster richColors/>
 
-      <p className="text-3xl text-blue-600 font-semibold">Is it a crime</p>
+      <main className="w-full">
 
-    </div>
+        <Routes>
+
+          <Route element={<Layout/>}>
+
+            <Route path="/" element={<Dashboard/>}/>
+
+          </Route>
+
+          <Route path="/sign-in" element={<SignIn/>}/>
+
+          <Route path="/sign-up" element={<SignUp/>}/>
+
+          <Route path="/forgot-password" element={<ForgotPassword/>}/>
+
+          <Route path="/reset-password/:token" element={<ResetPassword/>}/>
+
+        </Routes>
+
+      </main>
+
+    </BrowserRouter>
 
   )
 }
--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './redux/store.js'
+import { Provider } from 'react-redux'
+import StoreContextProvider from './context/store.jsx'
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
+
+    <PersistGate persistor={persistor}>
+
+      <Provider store={store}>
+
+        <StoreContextProvider>
+
+          <App />
+
+        </StoreContextProvider>
+
+      </Provider>
+
+    </PersistGate>
+
   </StrictMode>,
 )
