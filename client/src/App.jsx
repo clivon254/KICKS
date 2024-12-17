a2546407fa7c7f35bb5b512c2f1451acf46200f6
Clivon Osire <clivon254@icloud.com>
2024-12-17 17:51:35 +0300

    quick

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+
+export default function App() {
+
+  return (
+
+    <div>
+
+      <h2 className="text-3xl text-red-400 font-bold">Noma</h2>
+
+    </div>
+
+  )
+}
